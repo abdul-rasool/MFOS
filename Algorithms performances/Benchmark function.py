826b0017a8a85902f0fa1d5f30386a3b87c57580
Abdur Rasool <52996276+abdul-rasool@users.noreply.github.com>
2022-06-08 17:46:42 +0800

    Add files via upload

---
+++ Algorithms performances/Benchmark function.py
@@ -0,0 +1,232 @@
+
+import numpy
+import math
+
+# define the function blocks
+def prod(it):
+    p = 1
+    for n in it:
+        p *= n
+    return p
+
+
+def Ufun(x, a, k, m):
+    y = k * ((x - a) ** m) * (x > a) + k * ((-x - a) ** m) * (x < (-a))
+    return y
+
+
+def F1(x):
+    s = numpy.sum(x ** 2)
+    return s
+
+
+def F2(x):
+    o = sum(abs(x)) + prod(abs(x))
+    return o
+
+
+def F3(x):
+    dim = len(x) + 1
+    o = 0
+    for i in range(1, dim):
+        o = o + (numpy.sum(x[0:i])) ** 2
+    return o
+
+
+def F4(x):
+    o = max(abs(x))
+    return o
+
+
+def F5(x):
+    dim = len(x)
+    o = numpy.sum(
+        100 * (x[1:dim] - (x[0 : dim - 1] ** 2)) ** 2 + (x[0 : dim - 1] - 1) ** 2
+    )
+    return o
+
+
+def F6(x):
+    o = numpy.sum(abs((x + 0.5)) ** 2)
+    return o
+
+
+def F7(x):
+    dim = len(x)
+
+    w = [i for i in range(len(x))]
+    for i in range(0, dim):
+        w[i] = i + 1
+    o = numpy.sum(w * (x ** 4)) + numpy.random.uniform(0, 1)
+    return o
+
+
+def F8(x):
+    o = sum(-x * (numpy.sin(numpy.sqrt(abs(x)))))
+    return o
+
+
+def F9(x):
+    dim = len(x)
+    o = numpy.sum(x ** 2 - 10 * numpy.cos(2 * math.pi * x)) + 10 * dim
+    return o
+
+
+def F10(x):
+    dim = len(x)
+    o = (
+        -20 * numpy.exp(-0.2 * numpy.sqrt(numpy.sum(x ** 2) / dim))
+        - numpy.exp(numpy.sum(numpy.cos(2 * math.pi * x)) / dim)
+        + 20
+        + numpy.exp(1)
+    )
+    return o
+
+
+def F11(x):
+    dim = len(x)
+    w = [i for i in range(len(x))]
+    w = [i + 1 for i in w]
+    o = numpy.sum(x ** 2) / 4000 - prod(numpy.cos(x / numpy.sqrt(w))) + 1
+    return o
+
+
+def F12(x):
+    dim = len(x)
+    o = (math.pi / dim) * (
+        10 * ((numpy.sin(math.pi * (1 + (x[0] + 1) / 4))) ** 2)
+        + numpy.sum(
+            (((x[: dim - 1] + 1) / 4) ** 2)
+            * (1 + 10 * ((numpy.sin(math.pi * (1 + (x[1 :] + 1) / 4)))) ** 2)
+        )
+        + ((x[dim - 1] + 1) / 4) ** 2
+    ) + numpy.sum(Ufun(x, 10, 100, 4))
+    return o
+
+
+def F13(x):
+    if x.ndim==1:
+        x = x.reshape(1,-1)
+
+    o = 0.1 * (
+        (numpy.sin(3 * numpy.pi * x[:,0])) ** 2
+        + numpy.sum(
+            (x[:,:-1] - 1) ** 2
+            * (1 + (numpy.sin(3 * numpy.pi * x[:,1:])) ** 2), axis=1
+        )
+        + ((x[:,-1] - 1) ** 2) * (1 + (numpy.sin(2 * numpy.pi * x[:,-1])) ** 2)
+    ) + numpy.sum(Ufun(x, 5, 100, 4))
+    return o
+
+
+def F14(x):
+    aS = [
+        [
+            -32, -16, 0, 16, 32,
+            -32, -16, 0, 16, 32,
+            -32, -16, 0, 16, 32,
+            -32, -16, 0, 16, 32,
+            -32, -16, 0, 16, 32,
+        ],
+        [
+            -32, -32, -32, -32, -32,
+            -16, -16, -16, -16, -16,
+            0, 0, 0, 0, 0,
+            16, 16, 16, 16, 16,
+            32, 32, 32, 32, 32,
+        ],
+    ]
+    aS = numpy.asarray(aS)
+    bS = numpy.zeros(25)
+    v = numpy.matrix(x)
+    for i in range(0, 25):
+        H = v - aS[:, i]
+        bS[i] = numpy.sum((numpy.power(H, 6)))
+    w = [i for i in range(25)]
+    for i in range(0, 24):
+        w[i] = i + 1
+    o = ((1.0 / 500) + numpy.sum(1.0 / (w + bS))) ** (-1)
+    return o
+
+
+def F15(L):
+    aK = [
+        0.1957, 0.1947, 0.1735, 0.16, 0.0844, 0.0627,
+        0.0456, 0.0342, 0.0323, 0.0235, 0.0246,
+    ]
+    bK = [0.25, 0.5, 1, 2, 4, 6, 8, 10, 12, 14, 16]
+    aK = numpy.asarray(aK)
+    bK = numpy.asarray(bK)
+    bK = 1 / bK
+    fit = numpy.sum(
+        (aK - ((L[0] * (bK ** 2 + L[1] * bK)) / (bK ** 2 + L[2] * bK + L[3]))) ** 2
+    )
+    return fit
+
+
+def F16(L):
+    o = (
+        4 * (L[0] ** 2)
+        - 2.1 * (L[0] ** 4)
+        + (L[0] ** 6) / 3
+        + L[0] * L[1]
+        - 4 * (L[1] ** 2)
+        + 4 * (L[1] ** 4)
+    )
+    return o
+
+
+def F17(L):
+    o = (
+        (L[1] - (L[0] ** 2) * 5.1 / (4 * (numpy.pi ** 2)) + 5 / numpy.pi * L[0] - 6)
+        ** 2
+        + 10 * (1 - 1 / (8 * numpy.pi)) * numpy.cos(L[0])
+        + 10
+    )
+    return o
+
+
+def F18(L):
+    o = (
+        1
+        + (L[0] + L[1] + 1) ** 2
+        * (
+            19
+            - 14 * L[0]
+            + 3 * (L[0] ** 2)
+            - 14 * L[1]
+            + 6 * L[0] * L[1]
+            + 3 * L[1] ** 2
+        )
+    ) * (
+        30
+        + (2 * L[0] - 3 * L[1]) ** 2
+        * (
+            18
+            - 32 * L[0]
+            + 12 * (L[0] ** 2)
+            + 48 * L[1]
+            - 36 * L[0] * L[1]
+            + 27 * (L[1] ** 2)
+        )
+    )
+    return o
+
+
+# map the inputs to the function blocks
+def F19(L):
+    aH = [[3, 10, 30], [0.1, 10, 35], [3, 10, 30], [0.1, 10, 35]]
+    aH = numpy.asarray(aH)
+    cH = [1, 1.2, 3, 3.2]
+    cH = numpy.asarray(cH)
+    pH = [
+        [0.3689, 0.117, 0.2673],
+        [0.4699, 0.4387, 0.747],
+        [0.1091, 0.8732, 0.5547],
+        [0.03815, 0.5743, 0.8828],
+    ]
+    pH = numpy.asarray(pH)
+    o = 0
+    for i in range(0, 4):
+        o = o - cH[i] * numpy.exp(-(numpy.sum(aH[i, :] * ((L - pH[i, :]) ** 2))))
+    return o
